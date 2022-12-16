567b6ff5a60d66e65f4f0edfc8458670e3bb8e35
Cola <khoronenko24@gmail.com>
2022-12-16 19:16:18 +0200

    com

---
+++ pars.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+for p in range(1, 5):
+    result_list = []
+    r = requests.get(f'https://dom.ria.com/uk/arenda-kvartir/kiev?page={p}')
+    html = BS(r.content, 'html.parser')
+    prise = html.find_all("b", class_="size18")
+      
+    for el in prise:
+        result_list.append(el.text)
+
+print(result_list)
